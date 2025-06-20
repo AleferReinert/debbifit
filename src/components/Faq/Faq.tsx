62baf9178d3c00673dd6378eb3ef55b3f4e4c5c6
Alefer Reinert <aleferreinert@gmail.com>
2025-06-20 20:52:17 -0300

    feat(Faq): add title, add aria-hidden on svgs

--- src/components/Faq/Faq.tsx
+++ src/components/Faq/Faq.tsx
@@ -28,12 +28,13 @@ export function Faq({ title, questions }: FaqProps) {
             <li key={index}>
               <div
                 onClick={() => toggleAnswer(index)}
+                title={open ? 'Fechar' : 'Abrir'}
                 className='flex justify-between cursor-pointer items-center py-4'
               >
                 <h3 className='w-max pr-6 font-semibold'>
                   <span className='italic font-light pr-1'>{index + 1}.</span> {item.question}
                 </h3>
-                {open ? <IoChevronUpOutline /> : <IoChevronDownOutline />}
+                {open ? <IoChevronUpOutline aria-hidden /> : <IoChevronDownOutline aria-hidden />}
               </div>
               <div className={`${open ? 'max-h-96 transition-all' : 'max-h-0'} overflow-hidden duration-300`}>
                 <p className='px-6 pb-6 pt-2 font-light'>{item.answer}</p>
