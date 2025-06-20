9628a525db30ba298fc6ca0cc78b3c69329a21c4
Alefer Reinert <aleferreinert@gmail.com>
2025-06-20 14:06:31 -0300

    perf(SectionBenefits): set sizes to image

--- src/components/SectionBenefits/SectionBenefits.tsx
+++ src/components/SectionBenefits/SectionBenefits.tsx
@@ -20,7 +20,12 @@ export function SectionBenefits({ title, benefits }: SectionBenefitsProps) {
           return (
             <li key={index}>
               <div className='relative w-full aspect-video mb-6'>
-                <Image src={benefit.img.url} alt='' fill />
+                <Image
+                  src={benefit.img.url}
+                  alt=''
+                  fill
+                  sizes='(max-width: 640px) 288px, (max-width: 768px) 352px, (max-width: 1024px) 224px, 288px'
+                />
               </div>
               <h3 className='uppercase font-bold mb-1'>{benefit.title}</h3>
               <p className='font-light'>{benefit.description}</p>
