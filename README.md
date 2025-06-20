743e52fcf38974db0386bbc2a62a405b12ff900e
Alefer Reinert <aleferreinert@gmail.com>
2025-06-20 12:06:26 -0300

    feat: refactor Banner, remove plop and commitlint

--- README.md
+++ README.md
@@ -22,63 +22,35 @@ Landing page para consultoria fitness online.
    npm run dev
    ```
 
-# Scripts
+### Scripts
 
-### Commits
+1.  Iniciar Storybook:
 
-1. Adicione os arquivos:
+    ```
+    npm run storybook
+    ```
 
-   ```
-   git add .
-   ```
+2.  Executar testes:
 
-2. Selecione uma mensagem de commit válida:
+    ```
+    npm run test-storybook --watch
+    ```
 
-   ```
-   git commit
-   ```
+3.  Lint:
 
-### Storybook
-
-1. Iniciar:
-
-   ```
-   npm run storybook
-   ```
-
-2. Executar testes:
-   ```
-   npm run test --watch
-   ```
-
-### Lint
-
-```
-npm run lint
-```
-
-### Criar componente
-
-```
-npm run plop
-```
+    ```
+    npm run lint
+    ```
 
 ## Tecnologias Utilizadas
 
 - **Chromatic:** Ferramenta para captura de tela, revisão e testes visuais automatizados para componentes de interface de usuário.
-- **commitlint:** Ferramenta para validar e padronizar mensagens de commit.
-- **cz-commitlint:** Adaptador para Commitizen que aplica regras de mensagens de commit definidas pelo Commitlint.
 - **ESLint:** Ferramenta de linting para identificar e corrigir problemas em código JavaScript/TypeScript.
 - **Framer Motion:** Ferramenta para animações.
 - **Husky:** Ferramenta para adicionar hooks de Git, como pré-commit e pré-push, para automatizar verificações de código.
 - **Next.js:** Framework React para a construção de aplicações web estáticas e dinâmicas.
 - **React:** Biblioteca JavaScript para construir interfaces de usuário.
-- **Plop:** Ferramenta de scaffolding para gerar código e arquivos de forma consistente e automatizada.
 - **Prettier:** Formatador de código opinativo que garante um estilo consistente de código.
 - **Storybook:** Ferramenta de desenvolvimento para construir e testar componentes de interface de usuário de forma isolada.
 - **Tailwind CSS:** Framework de utilidades CSS para estilização rápida e eficiente.
 - **TypeScript:** Superconjunto de JavaScript que adiciona tipagem estática ao código.
-
-## URL do Projeto
-
-Explore o projeto no link: [DebbiFit](https://debbifit.vercel.app).
--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -1,14 +1,18 @@
-import { BannerImages, BannerImagesProps } from 'components/BannerImages/BannerImages'
 import { ButtonLink } from 'components/ButtonLink/ButtonLink'
 import { Container } from 'components/Container/Container'
+import Image from 'next/image'
 import { IoFitness } from 'react-icons/io5'
+import { UploadImageProps } from 'types/strapiContent'
 import { replacePTagsWithSpanTags } from 'utils/replacePTagsWithSpanTags'
 
-export interface BannerProps extends BannerImagesProps {
+export interface BannerProps {
   title: string
   description: string
   label: string
   url: string
+  floatImg: UploadImageProps
+  backgroundDesktop: UploadImageProps
+  backgroundMobile: UploadImageProps
 }
 
 export function Banner({ title, description, label, url, backgroundDesktop, backgroundMobile, floatImg }: BannerProps) {
@@ -16,8 +20,17 @@ export function Banner({ title, description, label, url, backgroundDesktop, back
     <section
       id='banner'
       data-testid='BannerComponent'
-      className='flex items-center relative bg-cover aspect-[9/16] sm:aspect-[16/9]'
+      className='flex items-center relative bg-cover aspect-[9/16] sm:aspect-video'
     >
+      <picture className='absolute top-0 left-0 right-0'>
+        <source media='(max-width: 639px)' srcSet={backgroundMobile.url} />
+        <img
+          src={backgroundDesktop.url}
+          alt={backgroundMobile.alternativeText || backgroundDesktop.alternativeText || 'Imagem de fundo decorativa'}
+          aria-hidden={backgroundMobile.alternativeText || backgroundDesktop.alternativeText ? false : true}
+        />
+      </picture>
+
       <Container className='relative flex gap-10 h-full'>
         <div className='z-10 flex items-center relative md:w-3/5'>
           <div className='w-full'>
@@ -37,7 +50,18 @@ export function Banner({ title, description, label, url, backgroundDesktop, back
             </ButtonLink>
           </div>
         </div>
-        <BannerImages floatImg={floatImg} backgroundDesktop={backgroundDesktop} backgroundMobile={backgroundMobile} />
+        <div className='hidden md:flex items-end w-2/5'>
+          <Image
+            aria-hidden={floatImg.alternativeText ? false : true}
+            src={floatImg.formats.small.url}
+            alt={floatImg.alternativeText || ''}
+            width={floatImg.width}
+            height={floatImg.height}
+            quality={100}
+            priority
+            className='max-w-full max-h-full'
+          />
+        </div>
       </Container>
       <div className='absolute inset-0 top-auto h-20 bg-gradient-to-t from-secondary-theme to-transparent' />
     </section>
